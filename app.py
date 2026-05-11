2182d2d8de416c603f5aa6caacd94dc64f5d854a
Hussein <bnsayd505@gmail.com>
2026-05-11 02:40:49 +0000

    fix and add project pages

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__Hussein__)
 
 projects = [
     {
@@ -46,3 +46,15 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    @app.route('/')
+def home():
+    return render_template('index.html', projects=projects)
+
+if __name__ == '__main__':
+    app.run(debug=True)@app.route('/project/neurotech')
+def neurotech():
+    return "<h1 style='text-align:center; margin-top:100px; font-family:sans-serif'>NeuroTech AI<br><br>قريباً 🚀</h1>"
+
+@app.route('/project/xo')
+def xo():
+    return "<h1 style='text-align:center; margin-top:100px; font-family:sans-serif'>XO Game AI<br><br>قريباً 🚀</h1>"
